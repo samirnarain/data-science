9dd69b12134624a9f8e8d66434e37df807a0b33a
samirnarain <50290853+samirnarain@users.noreply.github.com>
2019-09-19 14:13:36 +0200

    Update ass2.R

--- ass2.R
+++ ass2.R
@@ -3,6 +3,6 @@
 library("tidyverse")
 library("readr")
 
-df <- read_delim("BI_Raw_Data.csv", delim = ";", 
+df <- read_delim("data/BI_Raw_Data.csv", delim = ";", 
                  col_names = TRUE, col_types = NULL)
 
