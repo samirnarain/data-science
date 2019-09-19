51dde44b6734f00332f83843a6a9d50db68a0e4d
samirnarain <50290853+samirnarain@users.noreply.github.com>
2019-09-19 15:19:08 +0200

    Update ass2.R

--- dpv/ass2.R
+++ dpv/ass2.R
@@ -19,3 +19,34 @@ df %>%
   arrange(desc(Product_Order_Price_Total)) %>%
   head(5)
 
+# Step 2
+
+# Prepare the data
+
+product <- df %>%
+  select(Product_Name, Product_Category) %>%
+  rename(name = Product_Name, category = Product_Category) %>%
+  arrange(name, category) %>%
+  group_by(name, category) %>%
+  distinct() %>%
+  ungroup() %>%
+  mutate(productid = row_number())
+  
+
+customer <- df %>%
+  select(Customer_Name, Customer_Country) %>%
+  rename(name = Customer_Name, country = Customer_Country) %>%
+  arrange(name, country) %>%
+  group_by(name, country) %>%
+  distinct() %>%
+  ungroup() %>%
+  mutate(customerid = row_number())
+  
+
+# Sales data
+
+sales <- df %>%
+  select(Order_Date_Day, Customer_Name, Product_Name, Product_Order_Price_Total) %>%
+  full_join(product, by = c("Product_Name" = "name")) %>%
+  full_join(customer, by = c("Customer_Name" = "name")) %>%
+  select("Order_Date_Day","customerid", "productid", "Product_Order_Price_Total")
