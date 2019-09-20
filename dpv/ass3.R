ad746b3a40967d4bbeff752e068c808e9b5bb31c
samirnarain <50290853+samirnarain@users.noreply.github.com>
2019-09-20 14:05:38 +0200

    Update ass3.R

--- dpv/ass3.R
+++ dpv/ass3.R
@@ -1,6 +1,8 @@
 library("readr")
 library("dplyr")
 
+# using read.delim2 to parse European stype decimal place
+
 Main <- read.delim2("data/SuperstoreSales_main.csv",
                          sep = ';')
                          
@@ -11,10 +13,19 @@ Returns <- read.delim2("data/SuperstoreSales_returns.csv",
                        sep = ';')
 
 
+# top product category making losses
+
 Main %>%
   group_by(Product.Sub.Category) %>%
   summarise_at(vars(Profit), sum) %>%
   arrange(Profit) %>%
   head(5)
+
+# top products making loss
+
+Main %>%
+  group_by(Product.Name) %>%
+  summarise_at(vars(Profit), sum) %>%
+  arrange(Profit)
 
 
