cd0970ba68bcf8640cc344350c3e6807974f8313
samirnarain <50290853+samirnarain@users.noreply.github.com>
2019-10-08 09:24:12 +0200

    Update ass2.R

--- dm/ass2.R
+++ dm/ass2.R
@@ -1,10 +1,12 @@
 #install.packages("caret")
+#install.packages("e1071")
 
 library(readr)
 library(dplyr)
 library(caret)
 library(rpart)
 library(rpart.plot)
+library(e1071)
 
 df = read.csv(file = "data/births.csv")
 head(df)
@@ -26,26 +28,31 @@ df <- mutate(df, etni = if_else(etnicity == "Dutch",
 df$pari <- as.factor(df$pari)
 df$etni <- as.factor(df$etni)
 
+# we compute home and not at home as 
 # 0 is at home
 # 1 is not at home
+# initially it is stored in a temp column home01
+# we then copy home01 to home column
+# this is done because in glm, family="binomial" needs 0<=y<=1
 
 df <- mutate(df, home01 = if_else(df$home == "at_home", "0", "1" ))
 df$home01 <- as.numeric(df$home01)
-
+df$home <- df$home01
+df$home <- as.factor(df$home)
 
 head(df)
 str(df)
 
 # logistic regression model
 
-fit_lm <- glm(formula = home01 ~ pari + age_cat + etni + urban, 
+fit_lm <- glm(formula = home ~ pari + age_cat + etni + urban, 
               data = df, family = "binomial")
 
 summary(fit_lm)
 
 # decision tree model 
 
-fit_tree <- rpart(formula = home01 ~ pari + age_cat + etni + urban,
+fit_tree <- rpart(formula = home ~ pari + age_cat + etni + urban,
                   data= df , method = "class")
 
 summary(fit_tree)
@@ -54,44 +61,3 @@ summary(fit_tree)
 # 1 is not at home birth
 
 rpart.plot(fit_tree, fallen.leaves = T, type = 2)
-
-
-# training set
-
-set.seed(100)
-
-trainRowNumber <- createDataPartition(df$home, p=0.8, list = F)
-
-colnames(trainRowNumber)
-
-trainData <- df[trainRowNumber, c(2,4,8,9,10)]
-
-testData <- df[-trainRowNumber, c(2,4,8,9,10)]
-
-fit_logreg <- train(home ~ pari + age_cat + etni + urban,
-                    data=trainData, method = "glm") #, family = "binomial")
-
-predicted <- predict(fit_logreg, testData) 
-
-confusionMatrix(reference = testData$home, data = predicted,
-                mode='everything', positive='at_home')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
