47740cddada0175532594112f311505d5c3a9d1e
samirnarain <50290853+samirnarain@users.noreply.github.com>
2019-10-11 11:46:35 +0200

    Update project_samir.R

--- project/project_samir.R
+++ project/project_samir.R
@@ -1,21 +1,30 @@
+#install.packages("dplyr")
+#install.packages("ggplot2")
+#install.packages("readr")
+#install.packages("lubridate")
+#install.packages("RPostgreSQL")
+#install.packages("tidyverse")
+
+
 require(dplyr)
 require(ggplot2)
 require(readr)
+require(lubridate)
+require(RPostgreSQL)
+require(tidyverse)
 
 
 
 proj = read.csv2(file = "data/surgical_case_durations.csv")
 
-
 head(proj)
 str(proj)
+
 proj <- na.omit(proj)
+
 proj <- mutate(proj, time.diff = as.numeric(Geplande.operatieduur) - as.numeric(Operatieduur))
 
 ggplot(data = proj, aes(x=time.diff)) +
   geom_histogram(bins = 50)
 
-
 summary(proj$time.diff)
-
-
