56f135e2c7b4e2792b1b126c57b676a1a8e63786
TomWuerden <55581908+TomWuerden@users.noreply.github.com>
2019-11-04 19:28:19 +0100

    Update project.R

--- project/project.R
+++ project/project.R
@@ -18,84 +18,49 @@ library(rpart.plot)
 
 proj = read.csv2(file = "data/surgical_case_durations.csv")
 proj <- na.omit(proj)
-
 proj <- as_tibble(proj)
-
 head(proj)
 str(proj)
 
+
+#DATA PREPARATION (TRANSFORMING AND COMPUTING)
+
 # default coulumn type is factor
 # converting required coloumns to numeric for processing  
-
-proj$Leeftijd <- as.numeric(proj$Leeftijd)
-proj$Geplande.operatieduur  <- as.numeric(proj$Geplande.operatieduur)
+proj$Geplande.operatieduur <- as.numeric(proj$Geplande.operatieduur)
 proj$Operatieduur <- as.numeric(proj$Operatieduur)
-proj$BMI <- as.numeric(sub(",", ".", proj$BMI, fixed = TRUE))
-proj$Euroscore1 <- as.numeric(sub(",", ".", proj$Euroscore1, fixed = TRUE))
-proj$Euroscore2 <- as.numeric(sub(",", ".", proj$Euroscore2, fixed = TRUE))
-proj$Ziekenhuis.ligduur    <- as.numeric(proj$Ziekenhuis.ligduur)
+proj$BMI <- as.numeric(sub(",", ".", sub(".", "", proj$BMI, fixed=TRUE), fixed=TRUE))
+proj$Ziekenhuis.ligduur <- as.numeric(proj$Ziekenhuis.ligduur)
 proj$IC.ligduur <- as.numeric(proj$IC.ligduur)
+proj$Euroscore1 <- as.numeric(proj$Euroscore1)
+proj$Euroscore2 <- as.numeric(proj$Euroscore2)
+proj$Leeftijd <- as.numeric(proj$Leeftijd)
 
 
-# compute the time diff between planned duration and actual
+#compute time difference
 proj <- mutate(proj, time.diff = Operatieduur - Geplande.operatieduur)
-
-#see time difference
-ggplot(data = proj, aes(x=time.diff)) +
-  geom_histogram(bins = 50)
-
-
-
-#proj %>%
-#  filter(Leeftijd > 70)
-
-# to see impact of age on operationtime difference
-ggplot(proj, aes(x=Leeftijd, y=time.diff))+  
-  geom_point()+
-  geom_smooth()
 
 # compute age group
 proj <- proj %>%
   mutate(age_group = case_when(
-      Leeftijd > 70 ~ ">70",
-      Leeftijd > 60 & Leeftijd <= 70 ~ "60-70",
-      Leeftijd > 50 & Leeftijd <= 60 ~ "50-60",
-      Leeftijd > 40 & Leeftijd <= 50 ~ "40-50",
-      Leeftijd > 30 & Leeftijd <= 40 ~ "30-40",
-      Leeftijd > 20 & Leeftijd <= 30 ~ "20-30",
-      Leeftijd > 10 & Leeftijd <= 20 ~ "10-20",
-      Leeftijd <= 10 ~ "< 10"))
-
+    Leeftijd > 70 ~ ">70",
+    Leeftijd > 60 & Leeftijd <= 70 ~ "60-70",
+    Leeftijd > 50 & Leeftijd <= 60 ~ "50-60",
+    Leeftijd > 40 & Leeftijd <= 50 ~ "40-50",
+    Leeftijd > 30 & Leeftijd <= 40 ~ "30-40",
+    Leeftijd > 20 & Leeftijd <= 30 ~ "20-30",
+    Leeftijd > 10 & Leeftijd <= 20 ~ "10-20",
+    Leeftijd <= 10 ~ "< 10"))
 proj$age_group <- as.factor(proj$age_group)
 
 # compute BMI group
 proj <- proj %>%  
   mutate(BMI_group = case_when(
-      BMI >= 30              ~ "Obese",
-      BMI >= 25   & BMI < 30 ~ "Overweight",
-      BMI >= 18.5 & BMI < 25 ~ "Normal",
-      BMI <  18.5            ~ "Underweight",))
-     
-
+    BMI >= 30              ~ "Obese",
+    BMI >= 25   & BMI < 30 ~ "Overweight",
+    BMI >= 18.5 & BMI < 25 ~ "Normal",
+    BMI <  18.5            ~ "Underweight",))
 proj$BMI_group <- as.factor(proj$BMI_group)
-
-#upload to database
-
-#pw <- {
-#  "***"
-#}
-
-#drv <- dbDriver("PostgreSQL")
-#con <- dbConnect(drv, dbname = "dpv1a025",
-#                 host = "castle.ewi.utwente.nl", port = 5432,
-#                 user = "dpv1a025", password = pw,
-#                 options="-c search_path=project")
-#rm(pw)
-#
-#dbWriteTable(con, "Patients", value = proj, overwrite = T, row.names = F)
-
-summary(proj)
-
 
 # compute delay group
 proj <- proj %>%  
@@ -105,24 +70,14 @@ proj <- proj %>%
     time.diff >= -15 & time.diff < 15    ~ "Within 15 mins",
     time.diff >= -60 &  time.diff < -15  ~ "Early but <60min",
     time.diff <  -60                     ~ "Very Early"))
-
 proj$time_group <- as.factor(proj$time_group)
 
-# doesnt seem to be a helpful graph, but maybe we can try different variables for x & y
-proj %>%
-  ggplot(aes(x=time_group, y= Leeftijd)) +
-  geom_violin() +
-  scale_fill_viridis_c(alpha = .6)+
-  geom_jitter(color="black", size = 0.4, alpha=0.9)
-
-
-# write a coloums with the numver of operations planned
+# write a coloums with the number of operations planned
 proj <- mutate(proj, num_of_ops = str_count(proj$Operatietype, '\\+') + 1 )
 proj$num_of_ops <- as.factor(proj$num_of_ops)
 
 # attempting to separate the operations - WIP
 proj <- mutate(proj, op_types = sub(" + ", " , ", proj$Operatietype, fixed = TRUE)) 
-
 
 # compute Operation time group
 # doing this so it can be used as a factor. in its currenct form, operatieduur is a very large factor
@@ -137,15 +92,58 @@ proj <- proj %>%
     Operatieduur >=  120 & Operatieduur < 180    ~ "120-180",
     Operatieduur >=  60  & Operatieduur < 120    ~ "60-120",
     Operatieduur <  60                           ~ "<60"))
-
 proj$op_time_group <- as.factor(proj$op_time_group)
 
+
+
+
+
+
+
+
+
+
+
+#PLOTS
+
+ggplot(proj, aes(x=time.diff)) + geom_histogram(bins = 50)
+summary(proj$time.diff)
+summary(proj)
+glimpse(proj)
+
+#try to see if daytime impacts time diff
+# ggplot(proj, aes(x=Dagdeel, y=time.diff))+
+#   geom_bar(stat = "identity")
+
+# to see impact of age on operationtime difference
+ggplot(proj, aes(x=Leeftijd, y=time.diff))+  
+  geom_point()+
+  geom_smooth()
+
+# doesnt seem to be a helpful graph, but maybe we can try different variables for x & y
+proj %>%
+  ggplot(aes(x=time_group, y= Leeftijd)) +
+  geom_violin() +
+  scale_fill_viridis_c(alpha = .6)+
+  geom_jitter(color="black", size = 0.4, alpha=0.9)
+
+
+
+
+
+
+
+
+
+
+
 # Using Linear Regression and rpart on the data
+
 # we can use the actual time of the operation and see its relation with the other variables 
 
-#fit_lm <- glm(formula = Operatieduur ~ num_of_ops, data = proj, family = "binomial")
+fit_lm <- glm(formula = Operatieduur ~ num_of_ops, data = proj, family = "gaussian")
+summary(fit_lm)
 
-#summary(fit_lm)
 fit_tree <- rpart(formula = op_time_group ~ num_of_ops + age_group + Operatietype, 
                   data = proj,
                   method = "poisson")
@@ -156,3 +154,28 @@ rpart.plot(fit_tree, fallen.leaves = T, type = 2)
 plot(proj$time.diff~proj$num_of_ops , data = proj)
 
 plot(proj$Geplande.operatieduur,proj$time.diff)
+
+
+
+
+
+
+
+
+
+
+# UPLOAD
+# require("RPostgreSQL")
+# 
+# pw <- {
+#   "***"
+# }
+# 
+# drv <- dbDriver("PostgreSQL")
+# con <- dbConnect(drv, dbname = "dpv1a025",
+#                  host = "castle.ewi.utwente.nl", port = 5432,
+#                  user = "dpv1a025", password = pw,
+#                  options="-c search_path=project")
+# rm(pw)
+# 
+# dbWriteTable(con, "Patients", value = proj, overwrite = T, row.names = F)
